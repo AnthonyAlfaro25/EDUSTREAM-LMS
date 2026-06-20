c0f1b0e681e344c5105096b9fb53ad85aca2879c
AnthonyAlfaro25 <alfaroanthony205@gmail.com>
2026-06-20 13:16:49 -0600

    Resuelve conflicto: conserva Edustream.java

---
+++ src/main/java/com/vantylabs/edustream/Edustream.java
@@ -0,0 +1,9 @@
+package com.vantylabs.edustream;
+
+// Programa principal
+public class Edustream {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
