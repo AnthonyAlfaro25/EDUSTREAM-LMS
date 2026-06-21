c2967e661c1c68c28ddafc4acd0ceeaca99d4518
AnthonyAlfaro25 <alfaroanthony205@gmail.com>
2026-06-20 22:37:39 -0600

    anthony-feat: recreacion de la clases Estudiante, Profesor y Administrativo extendidas de Usuario

---
+++ src/main/java/com/vantylabs/edustream/Profesor.java
@@ -0,0 +1,26 @@
+package com.vantylabs.edustream;
+
+// @author anthony
+/* Clase para el usuario Profesor derivada de la clase abstracta Usuario */
+
+public class Profesor extends Usuario {
+    private String materia;
+
+    public Profesor(int id, String nombre, String email, String contrasenia, String materia) {
+        super(id, nombre, email, contrasenia);
+        this.materia = materia;
+    }
+
+    public String getMateria() { 
+       return materia; 
+    }
+    
+    public void setMateria(String materia) { 
+       this.materia = materia;
+    }
+
+    @Override
+    public String getRol() { 
+       return "Profesor";
+    }
+}
