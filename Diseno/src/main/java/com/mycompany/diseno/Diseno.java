d6522a83591a85cbce5ca6f525227343c48866c2
Manuel <manuetcrs@gmail.com>
2026-07-20 18:20:32 -0600

    Archivo de Prueba

---
+++ Diseno/src/main/java/com/mycompany/diseno/Diseno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.diseno;
+
+/**
+ *
+ * @author manue
+ */
+public class Diseno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Progra_2/src/main/java/com/mycompany/progra_2/Progra_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progra_2;
+
+/**
+ *
+ * @author manue
+ */
+public class Progra_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
